d03eac0c2e1fea4fa08136eeda0326adcfa6a961
archanVadgama <archan.vadgama@simformsolutions.com>
2025-03-18 17:06:43 +0530

    practical 1 and 2

---
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <h1>If you are reading this...</h1>
+    <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Books</li>
+      <li>Movies</li>
+      <li>Songs</li>
+    </ol>
+  `,
+  styles: ``,
+})
+export class AppComponent {}
