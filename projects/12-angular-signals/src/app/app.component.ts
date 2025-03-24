134bf58642d8c6bda1f67a3ab96498aed3e45d7e
archanVadgama <archan.vadgama@simformsolutions.com>
2025-03-24 16:07:05 +0530

    practical 12 and 13

--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
@@ -6,9 +6,33 @@ import { Todo } from './todo';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1>Building a TODO List</h1> `,
-  styles: `label { display: block }`,
+  template: `
+    <h1>Building a TODO List</h1>
+    <div *ngFor="let todo of todos()">
+      <label [ngStyle]="{ 'text-decoration': todo.completed ? 'line-through' : 'none' }">
+        <input type="checkbox" [checked]="todo.completed" (change)="updateTodo(todo)" />
+        {{ todo.title }}
+      </label>
+    </div>
+  `,
+  styles: [`label { display: block }`],
 })
 export class AppComponent {
-  updateTodo(todo: Todo) {}
+  todos = signal<Todo[]>([
+    { id: 1, title: 'Learn Angular', completed: false },
+    { id: 2, title: 'Learn TypeScript', completed: false },
+    { id: 3, title: 'Learn RxJS', completed: false },
+  ]);
+
+  updateTodo(todo: Todo) {
+    console.log(this.todos());
+    this.todos.update((todoList) =>
+      todoList.map((todoEntry) => {
+        if (todo.id === todoEntry.id) {
+          todoEntry.completed = !todoEntry.completed;
+        }
+        return todoEntry;
+      })
+    );
+  }
 }
--- projects/13-deferrable-views/src/app/app.component.ts
+++ projects/13-deferrable-views/src/app/app.component.ts
@@ -9,7 +9,15 @@ import { PostsComponent } from './posts/posts.component';
     <section class="container">
       <h1>Deferrable Views Example</h1>
       <button #loadPosts>Load Posts</button>
-      <app-posts />
+      @defer (on interaction(loadPosts)) {
+        <app-posts />
+      }@loading(minimum 1s) {
+        <h2>Loading...</h2>
+      }@error {
+        <h2>Something went wrong</h2>
+      }@placeholder {
+        <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"/>
+      }
     </section>
   `,
 })
